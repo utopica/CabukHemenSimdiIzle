619cdd3414f9b3ea8089457fa6d1b88f2a24595a
utopica <h.elifokumus@gmail.com>
2023-12-11 22:09:21 +0300

    [Feature] Created Common Interfaces
    Implemented entityBase and other common interfaces.

---
+++ Core/CabukHemenSimdiIzle.Domain/Common/IEntityBase.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace CabukHemenSimdiIzle.Domain.Common
+{
+    public interface IEntityBase<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ Core/CabukHemenSimdiIzle.Domain/Enums/Genre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabukHemenSimdiIzle.Domain.Enums
+{
+    public enum Genre
+    {
+        Action = 1,
+        Adventure = 2,
+        Animation = 3,
+        Comedy = 4,
+        Crime = 5,
+        Documentary = 6,
+        Drama = 7,
+        Family = 8,
+        Fantasy = 9,
+        History = 10,
+        Horror = 11,
+        Music = 12,
+        Mystery = 13,
+        Romance = 14,
+        ScienceFiction = 15,
+        Thriller = 16,
+        War = 17,
+        Western = 18,
+        
+    }
+
+}
