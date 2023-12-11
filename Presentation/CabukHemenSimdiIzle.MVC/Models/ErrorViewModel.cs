a65005327620c808fc16a2cf8c1a452f90019783
utopica <h.elifokumus@gmail.com>
2023-12-11 21:53:50 +0300

    Initial Create
    Project has been created.

---
+++ Presentation/CabukHemenSimdiIzle.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CabukHemenSimdiIzle.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
