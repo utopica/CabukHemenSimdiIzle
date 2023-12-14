abf0ed7c20c23780cd9e59b4d5d51d25af0b47b5
Cengizhan <civelekc18@itu.edu.tr>
2023-12-15 01:21:13 +0300

    [Fixed] Fixed registration operation.

--- Presentation/CabukHemenSimdiIzle.MVC/Program.cs
+++ Presentation/CabukHemenSimdiIzle.MVC/Program.cs
@@ -7,6 +7,12 @@ using CabukHemenSimdiIzle.Persistence.Contexts;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddControllersWithViews()
+    .AddNToastNotifyToastr();
+
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 //Adding DbContext
@@ -22,10 +28,6 @@ builder.Services.AddDbContext<IdentityDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
-// Add services to the container.
-builder.Services
-    .AddControllersWithViews()
-    .AddNToastNotifyToastr();
 
 
 //Add Identity System
@@ -45,7 +47,7 @@ builder.Services.AddIdentity<User, Role>(options =>
     options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@$";
     options.User.RequireUniqueEmail = true;
 
-}).AddEntityFrameworkStores<AppDbContext>()
+}).AddEntityFrameworkStores<IdentityDbContext>()
     .AddTokenProvider<DataProtectorTokenProvider<User>>(TokenOptions.DefaultProvider);
 
 builder.Services.Configure<SecurityStampValidatorOptions>(options =>
@@ -87,11 +89,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
